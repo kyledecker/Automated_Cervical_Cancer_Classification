d67b45a62931189a996c1788c87e2f520c529f63
wjl11@duke.edu <wjl11@duke.edu>
2016-12-04 16:52:45 -0500

    removed for loops in rgb pre processing for speed up

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ if __name__ == "__main__":
     from preprocess import read_tiff, rgb_histogram, rgb_preprocess
 
     rgb = read_tiff(filename='./test/ExampleAbnormalCervix.tif')
-    rgb = rgb_preprocess(rgb, verb=False, exclude_bg=True,
+    rgb = rgb_preprocess(rgb, verb=True, exclude_bg=True,
                                upper_lim=(0,  0,  150))
 
     rh, gh, bh = rgb_histogram(rgb, verb=True, omit=(0, 255))
