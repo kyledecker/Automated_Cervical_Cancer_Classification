8f83219b1dfa56bfc1844e2091f63154c21d4f7d
wjl11@duke.edu <wjl11@duke.edu>
2016-12-04 20:05:59 -0500

    added function to perform otsu's thresholding method

--- main.py
+++ main.py
@@ -6,9 +6,22 @@ sys.path.insert(0, os.path.abspath('./src/'))
 
 if __name__ == "__main__":
     from preprocess import read_tiff, rgb_histogram, rgb_preprocess
+    from feature_extraction import *
+
+    verb = False
 
     rgb = read_tiff(filename='./test/ExampleAbnormalCervix.tif')
-    rgb = rgb_preprocess(rgb, verb=True, exclude_bg=True,
-                               upper_lim=(0,  0,  240))
+    rgb = rgb_preprocess(rgb, verb=verb, exclude_bg=True, upper_lim=(0,  0,
+                                                                     240))
+    rh, gh, bh = rgb_histogram(rgb, verb=verb, omit=(0, 255))
 
-    rh, gh, bh = rgb_histogram(rgb, verb=True, omit=(0, 255))
+    green_otsu = otsu_threshold(rgb[:, :, 1], verb=verb)
+    blue_mode = calc_mode(bh)
+
+    msg = "G channel Otsu's threshold = %d" % green_otsu
+    logging.info(msg)
+    print(msg)
+
+    msg = "B channel mode = %d" % blue_mode
+    logging.info(msg)
+    print(msg)
--- src/feature_extraction.py
+++ src/feature_extraction.py
@@ -11,6 +11,49 @@ def calc_mode(hist):
     """
     import numpy as np
 
+    if np.ndim(hist) > 1:
+        msg = 'ERROR [calc_mode] Input must be 1D array of histogram ' \
+              'frequency values.'
+        logging.error(msg)
+        print(msg)
+        sys.exit()
+    if np.size(hist) > 256:
+        msg = 'ERROR [calc_mode] Input histogram data has > 256 bins.'
+        logging.error(msg)
+        print(msg)
+        sys.exit()
+
     mode = np.argmax(hist)
 
     return mode
+
+
+def otsu_threshold(img, verb=False):
+    """
+    calculate the global otsu's threshold for grayscale image
+
+    :param img: 2D array of pixel values
+    :param verb: verbose mode to display threshold image
+    :return: threshold (float)
+    """
+    from skimage.filters import threshold_otsu
+    import numpy as np
+
+    if np.ndim(img) != 2:
+        msg = 'ERROR [otsu_threshold] Input image must be 2D grayscale (not ' \
+              'RGB).'
+        logging.error(msg)
+        print(msg)
+        sys.exit()
+
+    # set omitted pixel values as 0
+    img[np.isnan(img)] = 0
+    threshold_global_otsu = threshold_otsu(img)
+
+    if verb:
+        import matplotlib.pyplot as plt
+        global_otsu = img >= threshold_global_otsu
+        plt.imshow(global_otsu, cmap=plt.cm.gray)
+        plt.show()
+
+    return threshold_global_otsu
