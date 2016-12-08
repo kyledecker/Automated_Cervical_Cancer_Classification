a9342d908b9c78d2bb07a9b19fb138aae0bf68b9
Kyle Decker <kyle.decker@duke.edu>
2016-12-08 11:32:00 -0500

    ROC and AUC metrics added. This closes #8

--- test/test_classification_model.py
+++ test/test_classification_model.py
@@ -2,7 +2,6 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('./src/'))
 
-
 def test_model_train_predict():
     from classification_model import train_model
     from classification_model import class_predict 
