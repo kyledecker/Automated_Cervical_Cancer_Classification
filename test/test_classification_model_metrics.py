c29398435867a50c1628715f012c3ce5837b9af7
wjl11@duke.edu <wjl11@duke.edu>
2016-12-08 14:58:33 -0500

    added function and unit test for generating confusion matrix (closes #10)

--- test/test_classification_model_metrics.py
+++ test/test_classification_model_metrics.py
@@ -2,13 +2,27 @@ import os
 import sys
 sys.path.insert(0, os.path.abspath('./src/'))
 
+
 def test_roc_auc():
     from classification_model_metrics import calc_ROC
     from classification_model_metrics import calc_AUC
     import numpy as np
 
-    targets = [0,1,0,1]
-    soft_predictions = [0.1,0.9,0.2,0.8]
-    #results = calc_ROC(targets,soft_predictions,False)
-    auc = calc_AUC(targets,soft_predictions)
+    targets = [0, 1, 0, 1]
+    soft_predictions = [0.1, 0.9, 0.2, 0.8]
+    #results = calc_ROC(targets, soft_predictions, False)
+    auc = calc_AUC(targets, soft_predictions)
     assert(auc == 1)
+
+
+def test_gen_confusion_matrix():
+    from classification_model_metrics import gen_confusion_matrix
+    import numpy as np
+
+    targets = [0, 1, 0, 1]
+    predictions = [0, 1, 0, 0]
+
+    actual = gen_confusion_matrix(targets, predictions, ['1', '2'], False)
+    expected = [[2, 0], [1, 1]]
+
+    assert np.array_equal(actual, expected)
