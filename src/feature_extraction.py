revised unit testing and routines for extract_features function (#22), fixed variance and median features to calculate values from pixels not histogram (closes #23)

src/feature_extraction.py
@@ -2,14 +2,16 @@ import sys
 import logging
 
 
-def calc_mode(hist):
+def calc_mode(hist, omit=[]):
     """
     calculate mode from histogram
 
     :param hist: histogram values for 0-255
+    :param omit: pixel values to omit from calculation
     :return: mode
     """
     import numpy as np
+    from accessory import get_iterable
 
     if np.ndim(hist) > 1:
         msg = 'ERROR [calc_mode] Input must be 1D array of histogram ' \
@@ -23,6 +25,9 @@ def calc_mode(hist):
         print(msg)
         sys.exit()
 
+    for omit_idx in get_iterable(omit):
+        hist[omit_idx] = 0
+
     mode = np.argmax(hist)
 
     return mode
@@ -30,7 +35,7 @@ def calc_mode(hist):
 
 def otsu_threshold(img, verb=False):
     """
-    calculate the global otsu's threshold for grayscale image
+    calculate the global otsu's threshold for image
 
     :param img: 2D array of pixel values
     :param verb: verbose mode to display threshold image
@@ -39,7 +44,7 @@ def otsu_threshold(img, verb=False):
     from skimage.filters import threshold_otsu
     import numpy as np
 
-    if np.ndim(img) != 2:
+    if np.ndim(img) > 2:
         msg = 'ERROR [otsu_threshold] Input image must be 2D grayscale (not ' \
               'RGB).'
         logging.error(msg)
@@ -59,61 +64,65 @@ def otsu_threshold(img, verb=False):
     return threshold_global_otsu
 
 
-def calc_median(hist):
+def calc_median(img, omit=[]):
     """
-    calculate median from histogram
+    calculate median pixel value of image
 
-    :param hist: histogram values for 0-255
+    :param img: 2D array of pixel values
+    :param omit: pixel values to omit from calculation
     :return: median
     """
     import numpy as np
+    from accessory import get_iterable
 
-    if np.ndim(hist) > 1:
-        msg = 'ERROR [calc_median] Input must be 1D array of histogram ' \
-              'frequency values.'
-        logging.error(msg)
-        print(msg)
-        sys.exit()
-    if np.size(hist) > 256:
-        msg = 'ERROR [calc_median] Input histogram data has > 256 bins.'
+    if np.ndim(img) > 2:
+        msg = 'ERROR [calc_median] Input image must be 2D grayscale (not ' \
+              'RGB).'
         logging.error(msg)
         print(msg)
         sys.exit()
 
-    median = np.argsort(hist)[len(hist)//2]
+    for omit_idx in get_iterable(omit):
+        img[img == omit_idx] = np.nan
+
+    pixels = np.ravel(img)
+    pixels = pixels[np.isfinite(pixels)]
+    median = np.median(pixels)
 
     return median
 
 
-def calc_variance(hist):
+def calc_variance(img, omit=[]):
     """
-    calculate variance from histogram
+    calculate variance of pixel values in image
 
-    :param hist: histogram values for 0-255
+    :param img: 2D array of pixel values
+    :param omit: pixel values to omit from calculation
     :return: variance
     """
     import numpy as np
+    from accessory import get_iterable
 
-    if np.ndim(hist) > 1:
-        msg = 'ERROR [calc_variance] Input must be 1D array of histogram ' \
-              'frequency values.'
-        logging.error(msg)
-        print(msg)
-        sys.exit()
-    if np.size(hist) > 256:
-        msg = 'ERROR [calc_variance] Input histogram data has > 256 bins.'
+    if np.ndim(img) > 2:
+        msg = 'ERROR [calc_variance] Input image must be 2D grayscale (not ' \
+              'RGB).'
         logging.error(msg)
         print(msg)
         sys.exit()
 
-    variance = np.std(hist)
+    for omit_idx in get_iterable(omit):
+        img[img == omit_idx] = np.nan
+
+    pixels = np.ravel(img)
+    pixels = pixels[np.isfinite(pixels)]
+    variance = np.var(pixels)
 
     return variance
 
 
 def extract_features(rgb, median_dims=None, variance_dims=None,
                      mode_dims=None, otsu_dims=None,
-                     hist_omit=[], verb=False):
+                     omit=[], verb=False):
     """
     extract specific image features from rgb pixel array
 
@@ -122,57 +131,67 @@ def extract_features(rgb, median_dims=None, variance_dims=None,
     :param variance_dims: color channel indices to extract variance feature
     :param mode_dims: color channel indices to extract mode feature
     :param otsu_dims: color channel indices to extract Otsu threshold
-    :param hist_omit: bins to omit from histogram feature extractions
+    :param omit: pixel values to omit from feature extraction
     :param verb: enable verbose mode to output intermediate figures
     :return: feature array (np.array)
     """
     from preprocess import rgb_histogram
+    from accessory import get_iterable
     import numpy as np
 
-    rh, gh, bh = rgb_histogram(rgb, verb=verb, omit=hist_omit)
+    rh, gh, bh = rgb_histogram(rgb, verb=verb, omit=omit)
     hists = (rh, gh, bh)
 
-    try:
-        median_feats = [calc_median(hists[ii]) for ii in median_dims]
-    except IndexError:
-        msg = 'ERROR [extract_features] Color channel index for median ' \
-              'feature out of bounds (0:R, 1:G, 2:B).'
-        logging.error(msg)
-        print(msg)
-        sys.exit()
-    except TypeError:
+    if median_dims is not None:
+        try:
+            median_feats = [calc_median(rgb[:, :, ii], omit) for ii in
+                            get_iterable(median_dims)]
+        except IndexError:
+            msg = 'ERROR [extract_features] Color channel index for median ' \
+                  'feature out of bounds (0:R, 1:G, 2:B).'
+            logging.error(msg)
+            print(msg)
+            sys.exit()
+    else:
         median_feats = []
-    try:
-        variance_feats = [calc_variance(hists[ii]) for ii in variance_dims]
-    except IndexError:
-        msg = 'ERROR [extract_features] Color channel index for variance ' \
-              'feature out of bounds (0:R, 1:G, 2:B).'
-        logging.error(msg)
-        print(msg)
-        sys.exit()
-    except TypeError:
+
+    if variance_dims is not None:
+        try:
+            variance_feats = [calc_variance(rgb[:, :, ii], omit) for ii in
+                              get_iterable(variance_dims)]
+        except IndexError:
+            msg = 'ERROR [extract_features] Color channel index for variance ' \
+                  'feature out of bounds (0:R, 1:G, 2:B).'
+            logging.error(msg)
+            print(msg)
+            sys.exit()
+    else:
         variance_feats = []
 
-    try:
-        mode_feats = [calc_mode(hists[ii]) for ii in mode_dims]
-    except IndexError:
-        msg = 'ERROR [extract_features] Color channel index for mode ' \
-              'feature out of bounds (0:R, 1:G, 2:B).'
-        logging.error(msg)
-        print(msg)
-        sys.exit()
-    except TypeError:
+    if mode_dims is not None:
+        try:
+            mode_feats = [calc_mode(hists[ii], omit) for ii in get_iterable(
+                mode_dims)]
+        except IndexError:
+            msg = 'ERROR [extract_features] Color channel index for mode ' \
+                  'feature out of bounds (0:R, 1:G, 2:B).'
+            logging.error(msg)
+            print(msg)
+            sys.exit()
+    else:
         mode_feats = []
 
-    try:
-        otsu_feats = [otsu_threshold(rgb[:, :, ii]) for ii in otsu_dims]
-    except IndexError:
-        msg = 'ERROR [extract_features] Color channel index for Otsu ' \
-              'threshold out of bounds (0:R, 1:G, 2:B).'
-        logging.error(msg)
-        print(msg)
-        sys.exit()
-    except TypeError:
+    if otsu_dims is not None:
+        try:
+            otsu_feats = [otsu_threshold(rgb[:, :, ii]) for ii in
+                          get_iterable(otsu_dims)]
+        except IndexError:
+            msg = 'ERROR [extract_features] Color channel index for Otsu ' \
+                  'threshold out of bounds (0:R, 1:G, 2:B).'
+            logging.error(msg)
+            print(msg)
+            sys.exit()
+    else:
         otsu_feats = []
 
     features = median_feats

src/preprocess.py
@@ -146,10 +146,11 @@ def process_rgb_histogram(hist, omit=[]):
     :return: normalized histogram (np.array)
     """
     import numpy as np
+    from accessory import get_iterable
 
     hist = np.array(hist).astype('float')
 
-    for ii in omit:
+    for ii in get_iterable(omit):
         hist[ii] = 0
 
     tot_pix = sum(hist)

test/test_feature_extraction.py
@@ -22,15 +22,10 @@ def test_calc_median():
     from feature_extraction import calc_median
     import numpy as np
 
-    hist = np.zeros(5)
-    hist[0] = 0.4
-    hist[1] = 0.5
-    hist[2] = 0.2
-    hist[3] = 0.9
-    hist[4] = 0.1
+    data = [ii for ii in range(0, 3)]
 
-    actual = calc_median(hist)
-    expected = 0
+    actual = calc_median(data)
+    expected = 1.
 
     assert actual == expected
 
@@ -39,15 +34,10 @@ def test_calc_variance():
     from feature_extraction import calc_variance
     import numpy as np
 
-    hist = np.zeros(5)
-    hist[0] = 0.4
-    hist[1] = 0.5
-    hist[2] = 0.2
-    hist[3] = 0.9
-    hist[4] = 0.1
+    data = [ii for ii in range(0, 4)]
 
-    actual = calc_variance(hist)
-    expected = np.std([0.4, 0.5, 0.2, 0.9, 0.1])
+    actual = calc_variance(data, omit=(0,))
+    expected = np.var([1, 2, 3])
 
     assert actual == expected
 
@@ -56,10 +46,22 @@ def test_extract_features():
     from feature_extraction import extract_features
     import numpy as np
 
-    img = np.zeros((20, 20, 3))
-    img[1, 1, :] = (0, 4, 4)
+    r = 5*np.ones([3, 3])
+    r[1, 1] = 0
+    g = np.zeros([3, 3])
+    b = [[ii for ii in range(0, 3)] for _ in range(0, 3)]
 
-    actual = extract_features(img, (0, 1), 0, (0, 1, 2))
-    expected = np.zeros(6)
+    img = np.zeros([3, 3, 3])
+    img[:, :, 0] = r
+    img[:, :, 1] = g
+    img[:, :, 2] = b
+
+    actual = extract_features(img, [1, 2], 0, [0, 1])
+    expected = [np.median(g), np.median(b), np.var(r), 5, 0]
+
+    assert np.array_equal(actual, expected)
+
+    actual = extract_features(img, 0, 0, 0, omit=0)
+    expected = [5, 0, 5]
 
     assert np.array_equal(actual, expected)