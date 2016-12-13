4ef4c6b325ab3ac387a6ecea636e3ea0f7eba17d
wjl11@duke.edu <wjl11@duke.edu>
2016-12-12 20:14:37 -0500

    updated visualize features wrapper with savefig and yellow pixel feature (closes #29)

--- visualize_features.py
+++ visualize_features.py
@@ -5,11 +5,12 @@ import numpy as np
 sys.path.insert(0, os.path.abspath('./src/'))
 
 
-def parse_cli():
+def parse_cli_visualizer():
     """parse CLI
 
     :returns: args
     """
+    from parse_cli import parse_bool
     import argparse as ap
     import os
 
@@ -21,34 +22,34 @@ def parse_cli():
                      help="Full Path to Training Data",
                      default=os.getcwd() + '/TrainingData/')
 
-    par.add_argument("--output_dir",
-                     dest="out_dir",
-                     help="Full Path to Output Directory",
-                     default=os.getcwd() + '/outputs/')
+    par.add_argument("--output_file",
+                     dest="out_file",
+                     help="Full Path to Output File",
+                     default=os.getcwd() + '/outputs/feature_plot.png')
 
-    par.add_argument("--median_features",
+    par.add_argument("--med",
                      dest="med_feats",
                      help="Color channels to extract median feature from <"
                           "rgb>",
                      default='')
 
-    par.add_argument("--variance_features",
+    par.add_argument("--var",
                      dest="var_feats",
                      help="Color channels to extract variance feature from <"
                           "rgb>",
                      default='')
 
-    par.add_argument("--mode_features",
+    par.add_argument("--mode",
                      dest="mode_feats",
                      help="Color channels to extract mode feature from <rgb>",
                      default='')
 
-    par.add_argument("--otsu_features",
+    par.add_argument("--otsu",
                      dest="otsu_feats",
                      help="Color channels to extract otsu feature from <rgb>",
                      default='')
 
-    par.add_argument("--yellow_feature",
+    par.add_argument("--y",
                      dest="ypct_feat",
                      help="Use percent yellow pixel feature <True/False>",
                      type=str,
@@ -60,31 +61,57 @@ def parse_cli():
     return args
 
 
-def parse_bool(inpar):
+def plot_features(features, targets, labels, outfile='feat_plot.png'):
     """
-    convert CLI True or False string into boolean
+    visualize 2D features for samples from 2 known classes
 
-    :param inpar: input True or False string (True/False or T/F)
-    :return: outpar (bool)
+    :param features: N x 2 sets of feature values
+    :param targets: target labels corresponding to each set of features
+    :param labels: labels for each feature
+    :param outfile: save location of output feature plot
     """
-    import sys
-    msg = 'ERROR [parse_cli] CLI must be either True/False. ' \
-          'Exiting script...'
+    import numpy as np
+    import matplotlib.pyplot as plt
+    from accessory import create_dir
 
-    try:
-        if inpar.lower() == 'true' or inpar.lower() == 't':
-            outpar = True
-        elif inpar.lower() == 'false' or inpar.lower() == 'f':
-            outpar = False
-        else:
-            print(msg)
-            sys.exit()
-
-    except ValueError:
+    target_types = np.unique(targets)
+    if len(target_types) > 2:
+        msg = 'ERROR [plot_features] Function only compatible with 2 targets.'
+        logging.error(msg)
         print(msg)
         sys.exit()
 
-    return outpar
+    if np.shape(features)[1] > 2:
+        msg = 'ERROR [plot_features] Function only compatible with 2 features.'
+        logging.error(msg)
+        print(msg)
+        sys.exit()
+
+    if np.shape(features)[0] != len(targets):
+        msg = 'ERROR [plot_features] Mismatch between number of target ' \
+              'labels and feature sets.'
+        logging.error(msg)
+        print(msg)
+        sys.exit()
+
+    features0 = features[targets == target_types[0], :]
+    features1 = features[targets == target_types[1], :]
+
+    h0 = plt.scatter(features0[:, 0], features0[:, 1], marker='o', c='red',
+                     label=target_types[0])
+    h1 = plt.scatter(features1[:, 0], features1[:, 1], marker='o',
+                     c='blue',
+                     label=target_types[1])
+    plt.xlabel(labels[0])
+    plt.ylabel(labels[1])
+    plt.legend(handles=[h0, h1], loc=4)
+    plt.grid(True)
+    plt.axis('tight')
+
+    create_dir(outfile)
+    msg = '[plot_features] Feature space plot saved: %s' % outfile
+    print(msg)
+    plt.savefig(outfile)
 
 
 if __name__ == "__main__":
@@ -93,8 +120,11 @@ if __name__ == "__main__":
     from classification_model import *
     from classification_model_metrics import *
 
-    args = parse_cli()
+    print('\nFEATURE SPACE VISUALIZATION')
+    args = parse_cli_visualizer()
     data_path = args.t_dir
+    outfile = args.out_file
+
     median_feats = args.med_feats
     variance_feats = args.var_feats
     mode_feats = args.mode_feats
@@ -103,6 +133,8 @@ if __name__ == "__main__":
     omit_pix = [0, 255]
 
     n_feat = len(median_feats+variance_feats+mode_feats+otsu_feats)
+    if ypct_feat:
+        n_feat += 1
 
     if n_feat != 2:
         msg = 'ERROR script compatible with only 2D feature sets. Actual # ' \
@@ -129,14 +161,14 @@ if __name__ == "__main__":
         rgb = rgb_preprocess(rgb, verb=False, exclude_bg=True,
                              upper_lim=(0, 0, b_thresh))
 
-        features = extract_features(rgb,
-                                    median_ch=median_feats,
-                                    variance_ch=variance_feats,
-                                    mode_ch=mode_feats,
-                                    otsu_ch=otsu_feats,
-                                    pct_yellow=ypct_feat,
-                                    omit=omit_pix,
-                                    verb=False)
+        features, l = extract_features(rgb,
+                                       median_ch=median_feats,
+                                       variance_ch=variance_feats,
+                                       mode_ch=mode_feats,
+                                       otsu_ch=otsu_feats,
+                                       pct_yellow=ypct_feat,
+                                       omit=omit_pix,
+                                       verb=False)
 
         feature_array[i, :] = features
 
@@ -145,4 +177,6 @@ if __name__ == "__main__":
         else:
             target_array[i] = -1
 
-    plot_features(feature_array, target_array, ['feature 1', 'feature 2'])
+    print('\nOUTPUTS')
+    plot_features(feature_array, target_array, l, outfile)
+    print('\n')
